b0f725b5a5662669162c285a5a4848018ffdf844
marijnj93 <marijn.j93@gmail.com>
2017-01-23 13:38:28 +0100

    Add main class
    Add main class and main method

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Parkeersimulator;
+/**
+ * Created by Marijn on 20-1-2017.
+ */
+public class Main {
+static Simulator sim;
+    public static void main(String[] args) {
+        sim = new Simulator();
+        sim.run();
+    }
+}
