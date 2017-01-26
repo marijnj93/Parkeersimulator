e0ec5b27a8a4d9acfe053eaf89059d5500f0b232
marijnj93 <marijn.j93@gmail.com>
2017-01-26 14:40:27 +0100

    Add javadoc to class CarQueue.
    Forgot to add javadoc to the class CarQueue, in he previous commit.

--- Parkeersimulator/CarQueue.java
+++ Parkeersimulator/CarQueue.java
@@ -5,14 +5,27 @@ import java.util.Queue;
 public class CarQueue {
     private Queue<Car> queue = new LinkedList<>();
 
+    /**
+     *
+     * @param car, A car to add to the queue.
+     * @return Add a car to the queue.
+     */
     public boolean addCar(Car car) {
         return queue.add(car);
     }
 
+    /**
+     *
+     * @return Retrieve and remove the head (first element) of this list.
+     */
     public Car removeCar() {
         return queue.poll();
     }
 
+    /**
+     *
+     * @return the size of the queue.
+     */
     public int carsInQueue(){
     	return queue.size();
     }
