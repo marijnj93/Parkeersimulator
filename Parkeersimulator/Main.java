d64c68884ef73dfd3c9b2c23062f195f4ddd9718
Mark Ma <mark-oranje@hotmail.com>
2017-01-26 13:52:08 +0100

    Revert "Revert "Test voor Bart""
    This reverts commit db4de1af46282c61e0d994ab2f4bbd1c848b0e9d.

--- Parkeersimulator/Main.java
+++ Parkeersimulator/Main.java
@@ -1,11 +1,10 @@
 package Parkeersimulator;
 /**
- * Created by Marijn on 20-1-2017.
+ * Created by Mark on 20-1-2017.
  */
 public class Main {
-static Simulator sim;
     public static void main(String[] args) {
-        sim = new Simulator();
+        Simulator sim = new Simulator();
         sim.run();
     }
 }
