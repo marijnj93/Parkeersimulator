a672ccf79c2cd057e0f876f1418a0c93d176f872
marijnj93 <marijn.j93@gmail.com>
2017-02-02 13:33:39 +0100

    Create a new Parkeergarage directly, without using a variable.

--- Parkeersimulator/runner/MVCRunner.java
+++ Parkeersimulator/runner/MVCRunner.java
@@ -10,6 +10,6 @@ import Parkeersimulator.main.Parkeergarage;
  */
 public class MVCRunner {
     public static void main(String[] args) {
-        Parkeergarage garage = new Parkeergarage();
+        new Parkeergarage();
     }
 }
