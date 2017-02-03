86ee68402a8ec3346550e8d78a43eb47d64c4abb
marijnj93 <marijn.j93@gmail.com>
2017-02-03 12:13:13 +0100

    Optimize access modifiers and add javadoc. Also change an instance variable to a local variable.

--- Parkeersimulator/controller/Settings.java
+++ Parkeersimulator/controller/Settings.java
@@ -10,19 +10,21 @@ import java.util.ArrayList;
 /**
  *
  * @author Marijn, Mark, Vincent, Bart,
- * @since 26-01-2017
+ * @version  26-01-2017
  */
 public class Settings extends Controller {
-
-    private ArrayList<Option> options = new ArrayList<Option>();
-
-
+    /**
+     * Create settings with options.
+     * @param simulator, A simulator.
+     * @param parkeergarage , A parkeergarage
+     */
     public Settings(Simulator simulator, Parkeergarage parkeergarage )  {
         super(simulator, parkeergarage);
 
         setLayout(new GridLayout(0, 3));
         setBorder(BorderFactory.createEmptyBorder(50, 50, 100, 100));
 
+        ArrayList<Option> options = new ArrayList<Option>();
         options.add(new Option("Enter speed: ", "enterSpeed"));
         options.add(new Option("Exit speed: : ", "exitSpeed"));
         options.add(new Option("Payment speed: : ", "paymentSpeed"));
@@ -37,24 +39,30 @@ public class Settings extends Controller {
         }
     }
 
-
-
     /**
      * Overridden. Tell the GUI manager how big we would like to be.
-     * @return The dimension of the GUI
+     * @return The dimension of the GUI.
      */
     public Dimension getPreferredSize() {
         repaint();
         return new Dimension(1200, 500);
     }
 
+    /**
+     * Class option, used for settings to configure the simulator.
+     */
     private class Option {
         private JLabel lbl_Description;
         private JTextField txt_Input;
         private JButton btn_Submit;
         private String variable;
 
-        public Option(String Description, String variable) {
+        /**
+         *
+         * @param Description, The description of a setting (label).
+         * @param variable, The name of a specific setting
+         */
+        Option(String Description, String variable) {
             lbl_Description = new JLabel(Description);
             txt_Input = new JTextField();
             btn_Submit = new JButton("Set");
@@ -70,14 +78,22 @@ public class Settings extends Controller {
                 setValue(value);
             });
         }
-        public void display() {
+
+        /**
+         * Display input fields, descriptions and a submit button.
+         */
+        void display() {
             add(lbl_Description);
             add(txt_Input);
             add(btn_Submit);
         }
 
+        /**
+         * Set the inputted value of the setting for the simulator.
+         * @param value, The value entered by a user to adjust the simulator.
+         */
         //Roep de juiste setter aan aan de hand van de in de constructor meegegeven variabele "variabele"
-        public void setValue(int value) {
+        void setValue(int value) {
             switch(variable) {
                 case "enterSpeed":
                     simulator.setEnterSpeed(value);
