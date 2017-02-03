461c79e32bb7772c61c5954f2f7766f15eb5c4fb
marijnj93 <marijn.j93@gmail.com>
2017-02-03 13:07:24 +0100

    Add / edit javadoc and optimize access modifiers.

--- Parkeersimulator/model/CarQueue.java
+++ Parkeersimulator/model/CarQueue.java
@@ -4,35 +4,40 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- *
+ * A queue for cars.
  * @author Marijn, Mark, Vincent, Bart,
  * @version 03-02-2017
- *
  */
 public class CarQueue {
     private Queue<Car> queue = new LinkedList<>();
 
     /**
-     *
+     * Add a car to the queue.
      * @param car, A car to add to the queue.
-     * @return Add a car to the queue.
+     * @return A queue with a car added.
      */
-    public boolean addCar(Car car) {
+    boolean addCar(Car car) {
         return queue.add(car);
     }
 
     /**
-     *
+     * Remove a car from the queue.
      * @return Retrieve and remove the head (first element) of this list.
      */
-    public Car removeCar() {
+    Car removeCar() {
         return queue.poll();
     }
 
-    public Car nextCar() {return queue.peek();}
+    /**
+     * Look at the next car in the queue.
+     * @return A car in the next position in the car.
+     */
+    Car nextCar() {
+        return queue.peek();
+    }
 
     /**
-     *
+     * Check the total number of cars in the queue.
      * @return the size of the queue.
      */
     public int carsInQueue(){
